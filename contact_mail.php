f0423bda1684208d25c4bc90a1247a63a54a6f5a
Jax <jaxmorgans92@gmail.com>
2017-07-19 17:26:37 +0100

    Update contact_mail.php

--- contact_mail.php
+++ contact_mail.php
@@ -1,18 +1,18 @@
 <?php
 
-$toEmail = "lmorgans90@gmail.com";
+$toEmail = "example@gmail.com";
 $mailHeaders = "From: " . $_POST["userName"] . "<". $_POST["userEmail"] .">\r\n";
 
 if(mail($toEmail, $_POST["subject"], $_POST["content"], $mailHeaders)) 
     
     { // if true, send html message to div 
-        print "<p class='success'>Your message has been sent, we will reply as soon as possible</p>";
+        print "<p>Your message has been sent, we will reply as soon as possible</p>";
     } 
 
 else // if false send html message to div
     
     {
-        print "<p class='Error'>Oops! There's been a problem, please try again!</p>";
+        print "<p>Oops! There's been a problem, please try again!</p>";
     }
 
 ?>
