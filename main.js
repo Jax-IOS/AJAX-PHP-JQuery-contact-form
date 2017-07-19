04878464685d53abc6717c8a24d9457adc4cffd5
Jax <jaxmorgans92@gmail.com>
2017-07-19 11:40:00 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function sendContact() {
+                
+    jQuery.ajax({
+    url: "contact_mail.php", // send information to php file
+    data:'userName='+$("#userName").val()+'&userEmail='+$("#userEmail").val()+'&subject='+$("#subject").val()+'&content='+$(content).val(),
+    type: "POST", // declare type of post
+    success:function(data){ // if successful (true) call function in if statement. Displayed to Div.
+    $("#mail-status").html(data);
+    },
+    error:function (){} // if failed (false) call function in if statement. Displayed to Div.
+    });
+
+}
